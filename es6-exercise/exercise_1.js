48fedea40aaf295250616afc9b481b1c3fd76642
Shubhamay Sarkar <shubsa15@in.ibm.com>
2024-06-01 18:10:01 +0530

    first commit

---
+++ es6-exercise/exercise_1.js
@@ -0,0 +1,14 @@
+
+// Example of Foreach
+function handlePosts() {
+    var posts = [
+      { id: 23, title: 'Daily JS News' },
+      { id: 52, title: 'Code Refactor City' },
+      { id: 105, title: 'The Brightest Ruby' }
+    ];
+    
+    posts.forEach(function(post) {
+      savePost(post);
+    });
+}
+
